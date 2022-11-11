e96bebed9f87fba88fb3c15f29571e518d223b3a
Nicolás Margenat <nmargenat@itba.edu.ar>
2022-11-11 20:16:20 -0300

    feat: sorting button

---
+++ app/src/main/java/com/example/fitness_first/ui/components/TopBarWFilter.kt
@@ -0,0 +1,70 @@
+package com.example.fitness_first.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.fitness_first.ui.theme.Secondary
+
+@Composable
+fun TopBarWFilter(onClickFilter: () -> Unit){
+    TopAppBar(
+        modifier = Modifier.height(128.dp),
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        Icons.Filled.Menu,
+                        contentDescription = "menu",
+                        tint = Secondary,
+                        modifier = Modifier.size(38.dp)
+                    )
+                }
+                IconButton(onClick = onClickFilter) {
+                    Icon(
+                        Icons.Filled.Person,
+                        contentDescription = "settings",
+                        tint = Secondary,
+                        modifier = Modifier.size(38.dp)
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Transparent),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                SearchBar()
+                IconFAB(
+                    icon = Icons.Filled.List,
+                    func = onClickFilter,
+                    modifier = Modifier.size(50.dp),
+                    backgroundColor = Secondary,
+                    contentColor = Color.Black
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/fitness_first/ui/screens/MyRoutinesScreen.kt
+++ app/src/main/java/com/example/fitness_first/ui/screens/MyRoutinesScreen.kt
@@ -4,20 +4,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.fitness_first.R
 import com.example.fitness_first.ui.components.BottomBar
 import com.example.fitness_first.ui.components.DetailedRoutineButton
-import com.example.fitness_first.ui.components.topBar
+import com.example.fitness_first.ui.components.TopBarWFilter
+import com.example.fitness_first.ui.theme.Quaternary
 import com.example.fitness_first.ui.theme.Secondary
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class BasicRoutineData
 constructor(name: String, category: String, liked: Boolean, func: (route: String) -> Unit = {}) {
@@ -27,49 +32,118 @@ constructor(name: String, category: String, liked: Boolean, func: (route: String
     val func = func
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MyRoutinesScreen(
     NavigateToRoutineDetails: (route: String) -> Unit,
     routineData: List<BasicRoutineData>,
     navController: NavHostController
 ) {
+    val sheetState = rememberBottomSheetState(
+        initialValue = BottomSheetValue.Collapsed
+    )
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = sheetState
+    )
+    val scope = rememberCoroutineScope()
     Scaffold(
-        topBar = {topBar()},
+        topBar = { TopBarWFilter(onClickFilter = { showFilters(scope = scope, sheetState = sheetState)}) },
         bottomBar = { BottomBar(navController = navController) }
     ){
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White),
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetContent = { sortSheet() },
+            sheetBackgroundColor = Quaternary,
+            sheetPeekHeight = 0.dp
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(6.dp)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White),
             ) {
-                Text(
-                    text = "My Routines",
-                    fontSize = MaterialTheme.typography.h4.fontSize,
-                    fontWeight = FontWeight.Bold,
-                    color = Secondary,
-                    modifier = Modifier.padding(start = 10.dp, top = 5.dp)
-                )
-                LazyColumn(
-                    modifier = Modifier.fillMaxWidth(),
+                Column(
+                    modifier = Modifier.fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+                    verticalArrangement = Arrangement.spacedBy(6.dp)
                 ) {
-                    items(routineData) {
+                    Text(
+                        text = "My Routines",
+                        fontSize = MaterialTheme.typography.h4.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Secondary,
+                        modifier = Modifier.padding(start = 10.dp, top = 5.dp)
+                    )
+                    LazyColumn(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+                    ) {
+                        items(routineData) {
                         /* TODO: aca deberias poder hacer detailedRoutineButton(data) */
                             data -> DetailedRoutineButton(
-                        name = data.name,
-                        category = data.category,
-                        liked = data.liked,
-                        func = { NavigateToRoutineDetails(data.name) } ,
-                    ) { }
+                                name = data.name,
+                                category = data.category,
+                                liked = data.liked,
+                                func = { NavigateToRoutineDetails(data.name) } ,
+                            ) { }
+                        }
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun sortSheet() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .padding(start = 8.dp, top = 4.dp)
+        ,
+        contentAlignment = Alignment.TopStart
+    ) {
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = stringResource(R.string.sort_by),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                )
+            }
+            val filters = listOf(
+                stringResource(R.string.date_up),
+                stringResource(R.string.date_down),
+                stringResource(R.string.rating_up),
+                stringResource(R.string.rating_down),
+                stringResource(R.string.diff_up),
+                stringResource(R.string.diff_down),
+            )
+            for (filter in filters) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(text = filter, fontSize = 24.sp)
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+private fun showFilters(scope: CoroutineScope, sheetState: BottomSheetState) {
+    scope.launch {
+        if (sheetState.isCollapsed) {
+            sheetState.expand()
+        } else {
+            sheetState.collapse()
         }
     }
 }
