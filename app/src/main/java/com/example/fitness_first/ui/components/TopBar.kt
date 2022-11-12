a74b7989a0dce6c6322f585e13f1ad6673ebf65b
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2022-11-12 16:14:33 -0300

    style: changed icon so that it is clearer with the background

--- app/src/main/java/com/example/fitness_first/ui/components/TopBar.kt
+++ app/src/main/java/com/example/fitness_first/ui/components/TopBar.kt
@@ -1,10 +1,9 @@
 package com.example.fitness_first.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
@@ -12,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -23,7 +23,9 @@ fun topBar(
     navController: NavHostController
 ){
     TopAppBar(
-        modifier = Modifier.height(128.dp).fillMaxWidth(),
+        modifier = Modifier
+            .height(128.dp)
+            .fillMaxWidth(),
         backgroundColor = Color.Transparent,
         elevation = 0.dp
     ) {
@@ -45,7 +47,13 @@ fun topBar(
                         modifier = Modifier.size(38.dp)
                     )
                 }
-                IconButton(onClick = { /*TODO*/ }) {
+                OutlinedButton(
+                    modifier = Modifier.size(50.dp),
+                    onClick = { navController.navigate("profile") },
+                    shape = CircleShape,
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.White),
+                    contentPadding = PaddingValues(0.dp)
+                ) {
                     Icon(
                         Icons.Filled.Person,
                         contentDescription = "settings",
@@ -55,7 +63,9 @@ fun topBar(
                 }
 
             }
-            Divider(color = Color.Gray, thickness = 2.dp, modifier = Modifier.fillMaxWidth().padding(top=5.dp, bottom = 10.dp))
+            Divider(color = Color.Gray, thickness = 2.dp, modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 5.dp, bottom = 10.dp))
             SearchBar(navController)
         }
     }
