fa53562230da5153ba979c3e16acd9385b6f64b8
MSGronda <mgronda@itba.edu.ar>
2022-11-13 14:38:29 -0300

    fix: fixed view model

--- app/src/main/java/com/example/fitness_first/MainActivity.kt
+++ app/src/main/java/com/example/fitness_first/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.fitness_first
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.fitness_first.ui.theme.FitnessfirstTheme
+import com.example.fitness_first.util.getViewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FitnessfirstTheme {
+                val viewModel: MainViewModel = viewModel(factory = getViewModelFactory())
                 AppNavHost()
             }
         }
