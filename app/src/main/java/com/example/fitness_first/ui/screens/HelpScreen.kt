f077d0c5390087a6571c372d4253debef2b2dc5a
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2022-11-17 11:38:46 -0300

    feat: help screen start of implementation

--- app/src/main/java/com/example/fitness_first/ui/screens/HelpScreen.kt
+++ app/src/main/java/com/example/fitness_first/ui/screens/HelpScreen.kt
@@ -1,15 +1,65 @@
 package com.example.fitness_first.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.fitness_first.R
+import com.example.fitness_first.ui.theme.Quaternary
+import com.example.fitness_first.ui.theme.Secondary
 
 @Composable
 fun HelpScreen() {
     Surface(
         modifier = Modifier.fillMaxSize(),
     ){
-        //TODO
+        Image(
+            painter = painterResource(id = R.drawable.bkg4), 
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Column (
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ){
+            Card(
+                backgroundColor = Quaternary
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(
+                        text = stringResource(R.string.faq),
+                        fontSize = MaterialTheme.typography.h5.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        color = Secondary,
+                        modifier = Modifier.padding(start = 20.dp, end = 20.dp, bottom = 10.dp, top = 10.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.create_routine),
+                        fontSize = MaterialTheme.typography.h5.fontSize,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.Black,
+                        modifier = Modifier.padding(start = 20.dp, end = 20.dp, bottom = 10.dp, top = 10.dp)
+                    )
+                }
+            }
+        }
     }
 }
